a297cf63f642d6dc2e5d06c5e59044024286bf30
Saad <reach.saad@outlook.com>
2025-09-29 22:39:00 +0530

    Settings+ShadCN

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: false,
+  output: 'export', // enables static HTML export
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
